10d628b2d5d3e08669a0fe2a5fa16a17448eb9d1
Freddyd95 <freddydiaz28@hotmail.com>
2024-03-06 21:52:12 -0800

    Adding styles to REST-rant

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express Settingsrs
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -15,4 +18,5 @@ app.get('*', (req, res) => {
     res.status(404) .send('<h1>404 Page</h1>')
 })
 
+// Listen for Connections
 app.listen(process.env.PORT)
