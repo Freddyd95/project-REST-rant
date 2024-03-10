08aaca03ebc6be756dd6acbf27fdee334a137724
Freddyd95 <freddydiaz28@hotmail.com>
2024-03-09 19:17:45 -0800

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -17,7 +17,33 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body)
+    if (!req.body.pic) {
+        //Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.cit) {
+        req.body.city = 'Anytown'
+    }
+    if(!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    places.redirect('/places')
     res.send('POST/places')
+})
+
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/edit', { place: places[id] })
+    }
+    res.render('places/edit')
 })
 
 module.exports = router 
