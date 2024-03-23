d6e26d428270dd32a7feb03dd8b4248d057e603b
Freddyd95 <freddydiaz28@hotmail.com>
2024-03-23 14:11:10 -0700

    Added ratings to Show Page

--- views/show.jsx
+++ views/show.jsx
@@ -7,11 +7,31 @@ function show (data) {
             No Comments Yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated 
+        </h3>
+    )
+    if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce(( tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+            stars += '⭐️'
+        }
+        rating = (
+            <h3>
+                {stars} stars
+            </h3>
+        )
+    }
     if (data.place.comments.length) {
         comments = data.place.comments.map( c => {
             return (
                 <div className="border">
-                    <h2 className="rant"> {c.rant ? 'Rant! ' : 'Rave! '}</h2>
+                    <h2 className="rant"> {c.rant ? 'Rant! 😡' : 'Rave! 😍'}</h2>
                     <h4>{c.content}</h4>
                     <h3>
                         <strong>- {c.author}</strong>
@@ -46,6 +66,7 @@ function show (data) {
                 <h1>{ data.place.name }</h1>
                 <div>
                 <h2>Rating</h2>
+                {rating}
                 <p>Not Rated</p>
                 </div>
                 <div>
